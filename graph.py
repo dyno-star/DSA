40627a02fe2c49c959a56d2a807fe2aa716b1b8a
dyno-star <da893@drexel.edu>
2025-05-22 10:19:10 -0400

    graph

--- graph.py
+++ graph.py
@@ -11,6 +11,8 @@ class Graph:
     def add_vertex(self,vertex):
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex]= []
+            return True
+        return False
     
     def add_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
